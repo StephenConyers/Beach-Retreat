e57437cdc64a1e9ee32a4d32c9899c09a1f43e4d
StephenConyers <sconyers28@gmail.com>
2019-03-11 00:06:28 +0000

    Add files via upload

---
+++ Content/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+    $(document).on('click', 'a[href^="#"]:not(.carousel-control)', function () {
+        if (!/#/.test(this.href)) return;
+        var id = $(this).attr('href');
+        if (id === '#') return;
+        $('html,body').animate({
+            'scrollTop': $(id).offset().top - 80
+        }, 1000);
+        return false;
+    });
+
+    if (document.addEventListener) {
+        var wow = new WOW().init();
+    }
+});
