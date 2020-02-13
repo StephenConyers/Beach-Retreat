043e3142f64d885e51a1d26235aeda61c673025f
StephenConyers <sconyers28@gmail.com>
2020-02-13 22:41:24 +0000

    Add files via upload

--- Content/js/main.js
+++ Content/js/main.js
@@ -12,4 +12,28 @@ $(document).ready(function () {
     if (document.addEventListener) {
         var wow = new WOW().init();
     }
+
+    var resizeCarousel = function () {
+        var wHeight = $(window).height();
+        var navHeight = $('[fh-navbar]').outerHeight(true);
+        var item = $('.carousel-item:first', '[fh-carousel]');
+        var item2 = $('.carousel-inner', '[fh-carousel]');
+        $('.carousel-item', '[fh-carousel]').height(wHeight - navHeight - (parseInt(item.css('paddingTop'), 10) + parseInt(item.css('paddingBottom'), 10)));
+        $('.carousel-inner', '[fh-carousel]').height(wHeight - navHeight - (parseInt(item2.css('paddingTop'), 10) + parseInt(item2.css('paddingBottom'), 10)));
+    };
+
+    $(window).on('resize load', resizeCarousel);
+    resizeCarousel();
+
+    var resizeMap = function () {
+        var wHeight = $(window).height();
+        var navHeight = $('[fh-navbar]').outerHeight(true);
+        var item = $('iframe', '#location');
+        var item2 = $('.container-fluid', '#location');
+        $('iframe', '#location').height(wHeight - navHeight - (parseInt(item.css('paddingTop'), 10) + parseInt(item.css('paddingBottom'), 10)));
+        $('.container-fluid', '#location').height(wHeight - navHeight - (parseInt(item2.css('paddingTop'), 10) + parseInt(item2.css('paddingBottom'), 10)));
+    };
+
+    $(window).on('resize load', resizeMap);
+    resizeMap();
 });
